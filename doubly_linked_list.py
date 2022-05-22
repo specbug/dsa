8646fbb56a232257a84edb3b06672c9594a79a34
specbug <rishit7.rv@gmail.com>
2022-05-22 17:04:36 +0530

    dll gc

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -22,88 +22,88 @@ class DoublyLinkedList(object):
             this = this.next
         return _repr.rstrip(' <-> ')
 
-    def append(self, value: Any):
-        new_node = Node(value, None, None)
-        if self.tail:
-            tail = self.tail
-            self.tail = new_node
-            tail.next = self.tail
-            self.tail.prev = tail
-            return
-        this = self.head
-        if this is None:
-            self.insert_at_beginning(value)
-            return
-        while this.next:
-            this = this.next
-        this.next = new_node
-        self.tail = new_node
-        self.tail.prev = this
-
     def insert_at_beginning(self, value: Any):
         head = Node(value, None, None)
         if self.head:
             head.next = self.head
-            head.next.prev = head
+            self.head.prev = head
         self.head = head
+        if not self.tail:
+            self.tail = head
 
     def insert_at_end(self, value: Any):
-        self.insert_after(value, self.tail)
+        tail = Node(value, self.tail, None)
+        if self.tail:
+            self.tail.next = tail
+        self.tail = tail
+        if not self.head:
+            self.head = tail
 
-    def insert_after(self, value: Any, after: 'Node'):
-        new_node = Node(value, after, after.next)
-        tail = new_node
-        if after != self.tail:
-            tail = None
-            after.next.prev = new_node
-        after.next = new_node
-        self.tail = tail or self.tail
+    def insert_after(self, value: Any, after: Any):
+        new_node = Node(value, None, None)
+        this = self.head
+        while this.next:
+            if this.value == after:
+                new_node.next = this.next
+                new_node.prev = this
+                this.next.prev = new_node
+                this.next = new_node
+                return
+            this = this.next
+        self.insert_at_end(value)
 
-    def insert_before(self, value: Any, before: 'Node'):
-        if before == self.head:
-            self.insert_at_beginning(value)
-        else:
-            new_node = Node(value, before.prev, before)
-            before.prev.next = new_node
-            before.prev = new_node
+    def insert_before(self, value: Any, before: Any):
+        new_node = Node(value, None, None)
+        this = self.head
+        while this:
+            if this.value == before:
+                new_node.next = this
+                new_node.prev = this.prev
+                if this == self.head:
+                    self.insert_at_beginning(value)
+                    return
+                this.prev.next = new_node
+                this.prev = new_node
+                return
+            this = this.next
 
-    def insert(self, value: Any, after: 'Node' = None, before: 'Node' = None):
-        if not (after or before):
-            self.insert_at_beginning(value)
-        elif not after:
-            self.insert_after(value, after)
-        else:
-            self.insert_before(value, before)
+    def remove_from_beginning(self):
+        self.head = self.head.next
+        self.head.prev = None
+
+    def remove_from_end(self):
+        self.tail = self.tail.prev
+        self.tail.next = None
 
     def remove(self, value: Any):
         this = self.head
-        while this.next:
-            if this.value == value:
-                if this.prev:
-                    this.prev.next = this.next
-                else:
-                    self.head = this.next
+        while this:
+            if value == this.value:
+                if this == self.head:
+                    self.remove_from_beginning()
+                    return
+                if this == self.tail:
+                    self.remove_from_end()
+                    return
+                this.prev.next = this.next
                 this.next.prev = this.prev
                 return
             this = this.next
-        if value == this.value:
-            self.tail = this.prev
-            self.tail.next = None
 
 
 if __name__ == '__main__':
     dll = DoublyLinkedList()
-    dll.append(1)
-    dll.append(2)
-    dll.append(3)
-    dll.append(4)
-    dll.append(5)
+    dll.insert_at_beginning(1)
+    dll.insert_at_end(2)
+    dll.insert_at_end(3)
+    dll.insert_at_end(4)
+    dll.insert_at_end(5)
     print(dll)
     print('inserting 0 at beginning')
-    dll.insert(0)
+    dll.insert_at_beginning(0)
     print(dll)
     print('inserting 6 at end')
-    dll.append(6)
+    dll.insert_at_end(6)
     print(dll)
     print('removing 3')
     dll.remove(3)
@@ -111,18 +111,30 @@ if __name__ == '__main__':
     print('removing 6')
     dll.remove(6)
     print(dll)
-    print('removing 5')
-    dll.remove(5)
+    print('inserting 8 after 4')
+    dll.insert_after(8, 4)
     print(dll)
-    print('appending 8')
+    print('inserting 8 before 4')
+    dll.insert_before(8, 4)
+    print(dll)
+    print('removing 5 from end')
+    dll.remove_from_end()
+    print(dll)
+    print('inserting 8 at end')
     dll.insert_at_end(8)
     print(dll)
     print('insert 1 at beginning')
     dll.insert_at_beginning(1)
     print(dll)
-    print('delete 0 & 1')
+    print('delete 0')
     dll.remove(0)
-    dll.remove(1)
+    print(dll)
+    print('removing 1 from beginning')
+    dll.remove_from_beginning()
+    print(dll)
+    print('removing two 8s')
+    dll.remove(8)
+    dll.remove(8)
     print(dll)
     print(f'head: {dll.head.value}')
     print(f'tail: {dll.tail.value}')
